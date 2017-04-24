ddb36449209d13b3b3f3dc01b6908c0a6eaeddc7
Guillaume Orsal <guillaume@orsal.net>
2017-04-25 00:05:11 +0200

    Bug dans la détection d'une nouvelle machine

--- start_server.sh
+++ start_server.sh
@@ -4,10 +4,9 @@
 # Permet de lancer les serveurs qui ne sont pas lancés automatiquement
 
 # On vérifie que la machine est installée
-if [ -e /usr/local/bin/composer -a -e /etc/supervisord.conf ]
+if [ -e /usr/local/bin/composer ]
 then
    sudo service apache2 restart
-   supervisord -u vagrant -q /vagrant/log/supervisor
 else
    echo "Cette machine semble neuve. Pour l'installer:"
    echo "install_inside.sh"
